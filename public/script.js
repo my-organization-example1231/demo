07005ce6022ab4b31f7af51c2187b72f287f93ae
ArwaIndonesia <arwa.lanetteam@gmail.com>
2024-04-10 12:32:03 +0530

    test

--- public/script.js
+++ public/script.js
@@ -1,36 +1,11 @@
 function createForm(conf) {
     window.addEventListener('load', function () {
-        function isParentWindowResized() {
-            var initialWidth = window.innerWidth;
-            var initialHeight = window.innerHeight;
         
-            // Function to check if the window has been resized after a short delay
-            function checkResize() {
-                if (window.innerWidth !== initialWidth || window.innerHeight !== initialHeight) {
-                    return true; // Window has been resized
-                }
-                return false; // Window has not been resized
-            }
-        
-            // Check if the window has been resized after a short delay
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    resolve(checkResize());
-                }, 1000); // Adjust the delay as needed
-            });
-        }
-        isParentWindowResized().then(resized => {
-            if (!resized) {
-                const div = document.createElement('div');
-                div.style.height = '100%';
-                var element = document.getElementById(`${conf?.scriptId}`);
-                div.innerHTML = `<iframe src=https://form.crmone.com/form/${conf?.formId} frameborder="0"
-                            allowtransparency="true" loading="lazy" width="100%" height="100%"></iframe>`;
-                element.insertAdjacentElement('afterend', div);
-            } else {
-                console.log('Iframe not loaded as parent window was resized.');
-            }
-        });
+        const div = document.createElement('div');
+        div.style.height = '100%';
+        var element = document.getElementById(`${conf?.scriptId}`);
+        div.innerHTML = `<embed src=https://form.crmone.com/form/${conf?.formId} width="100%" height="100%"></embed>`;
+        element.insertAdjacentElement('afterend', div);
     });
 }
 
