5ebb50cbe65b8aeddda44794470b25ae193915ae
ArwaIndonesia <arwa.lanetteam@gmail.com>
2024-04-10 11:24:02 +0530

    test

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 function createForm(conf) {
     window.addEventListener('load', function () {
         
-        // const div = document.createElement('div');
-        // div.style.height = '100%';
-        // var element = document.getElementById(`${conf?.scriptId}`);
-        // div.innerHTML = `<iframe src=https://form.crmone.com/form/${conf?.formId} frameborder="0"
-        //             allowtransparency="true" loading="lazy" width="100%" height="100%"></iframe>`;
-        // element.insertAdjacentElement('afterend', div);
+        const div = document.createElement('div');
+        div.style.height = '100%';
+        var element = document.getElementById(`${conf?.scriptId}`);
+        div.innerHTML = `<iframe src=https://form.crmone.com/form/${conf?.formId} frameborder="0"
+                    allowtransparency="true" loading="lazy" width="100%" height="100%"></iframe>`;
+        element.insertAdjacentElement('afterend', div);
     });
 }
 
